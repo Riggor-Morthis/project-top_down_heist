857b8730af26f82cf9b998b1e12f27384725cd48
Riggor Morthis <roc.syl@outlook.com>
2022-05-09 23:55:17 +0200

    09/05
    Systeme de gravite fonctionnel, plus qu'a creer un terrain pour les tester

--- Assets/Scripts/Physics/WheelsContactEffectScript.cs
+++ Assets/Scripts/Physics/WheelsContactEffectScript.cs
@@ -11,18 +11,22 @@ public class WheelsContactEffectScript : MonoBehaviour
     [SerializeReference, Tooltip("Les autres roues du vehicule, si necessaire")]
     private Collider[] remainingWheels; //Pour rappel, les roues sont dans l'ordre gauche, droite, gauche, droite...
 
-    private WheelContactScript[] wheelContacts;
-    private int index;
+    private WheelContactScript[] wheelContacts; //La liste des scipts de contact presents sur le vehicule
+    private int index; //Un index pour parcourir la liste du dessus
 
-    private bool onTheGround = false;
-    private float torqueMultiplier, forwardScalar, turningScalar;
+    private bool onTheGround = false; //Est-ce qu'on est au sol ?
+    private int drivingOnTheGround = 0, steeringOnTheGround = 0; //Combien de roues de chaque type sont au sol
+    private float torqueMultiplier, forwardScalar, turningScalar; //Des scalaires qu'on calcule a partir de notre situation actuelle
     #endregion
 
     #region Start
     private void Start()
     {
+        //On instancie nos variables
         wheelContacts = new WheelContactScript[drivingWheels.Length + steeringWheels.Length + remainingWheels.Length];
         index = 0;
+
+        //On utilise les listes specifiees par l'utilisateur pour remplir la liste du dessus
         ExtractList(drivingWheels);
         ExtractList(steeringWheels);
         ExtractList(remainingWheels);
@@ -30,40 +34,67 @@ public class WheelsContactEffectScript : MonoBehaviour
     #endregion
 
     #region Public_Methods
+    /// <summary>
+    /// Permet de calculer les effets qui s'appliquent au comportement du vehicule en fonction des roues qui touchent le sol (ou non)
+    /// </summary>
     public void ContactEffects()
     {
+        //On passera a false des qu'une roue est en l'air
         onTheGround = true;
 
+        //On commence par verifier l'etat des roues motrices
+        drivingOnTheGround = drivingWheels.Length;
         torqueMultiplier = 0;
         forwardScalar = 0;
         index = 0;
+        //On fait le tour des roues motrices
         while(index < drivingWheels.Length)
         {
+            //Si elle est au sol...
             if (wheelContacts[index].isContacting())
             {
+                //... sa puissance peut s'appliquer au vehicule
                 torqueMultiplier++;
-                if (index % 2 == 0) forwardScalar--;
-                else forwardScalar++;
+                if (index % 2 == 0) forwardScalar++; //Pour rappel, les roues paires sont a gauche...
+                else forwardScalar--;//... les roues impaires sont a droite
             }
-            else onTheGround = false;
+            //Si elle est pas au sol, on en tient compte dans nos variables de contact
+            else
+            {
+                onTheGround = false;
+                drivingOnTheGround--;
+            }
             index++;
         }
+        //On finit de calculer les variables de comportement pour notre vehicule
         torqueMultiplier /= drivingWheels.Length;
         forwardScalar /= drivingWheels.Length;
 
+        //On travaille sur l'effet des roues directrices
+        steeringOnTheGround = steeringWheels.Length;
         turningScalar = 0;
+        //On fait le tour des roues directrices
         while(index < drivingWheels.Length + steeringWheels.Length)
         {
+            //Si elle est au sol...
             if (wheelContacts[index].isContacting())
             {
-                if (index % 2 == 0) turningScalar--;
-                else turningScalar++;
+                //...sa direction peut s'appliquer au vehicule
+                if (index % 2 == 0) turningScalar++;//Pour rappel, les roues paires sont a gauche...
+                else turningScalar--;//... les roues impaires sont a droites
             }
-            else onTheGround = false;
+            //Si elle est pas au sol, on en tient compte dans nos variables de contact
+            else
+            {
+                onTheGround = false;
+                steeringOnTheGround--;
+            }
             index++;
         }
+        //On finit de calculer les variables de comportement pour notre vehicule
         turningScalar /= steeringWheels.Length;
 
+        //On finit par verifier les roues inutiles, des fois qu'une d'entre elles ne soit pas au sol
         while (index < drivingWheels.Length + steeringWheels.Length + remainingWheels.Length)
         {
             if (!wheelContacts[index].isContacting()) onTheGround = false;
@@ -72,14 +103,21 @@ public class WheelsContactEffectScript : MonoBehaviour
     }
 
     public bool IsOnTheGround() => onTheGround;
+    public bool IsDrivingOnTheGround() => drivingOnTheGround > 0;
+    public bool IsSteeringOnTheGround() => steeringOnTheGround > 0;
     public float GetTorqueMultiplier() => torqueMultiplier;
     public float GetForwardScalar() => forwardScalar;
     public float GetTurningScalar() => turningScalar;
     #endregion
 
     #region Private_Methods
+    /// <summary>
+    /// Permet d'extraire des components d'une roue pour les placer dans la liste appropriee
+    /// </summary>
+    /// <param name="wheels">Les roues sur lesquelles on veut faire l'extraction</param>
     private void ExtractList(Collider[] wheels)
     {
+        //Pour chaque roue, on extrait le bon script pour le placer dans la liste centrale
         foreach (Collider wheel in wheels)
         {
             wheelContacts[index] = wheel.GetComponent<WheelContactScript>();
--- Assets/Scripts/Vehicles/VehicleControlScript.cs
+++ Assets/Scripts/Vehicles/VehicleControlScript.cs
@@ -16,7 +16,7 @@ public class VehicleControlScript : MonoBehaviour
     private float statDisplacement = 1397, realDisplacement; //La cylindree au joueur, et la cylindree utilisee par ce script
     private float statBrake = 66, realBrake; //Les freins annoncee au joueur, et les freins utilises par ce script
     private float statHandling = 115, realHandling; //Le maniement annonce au joueur, et le maniement utilise par ce script
-    private float realWeight = 962; //La seule stat du joueur qui ne demande pas d'etre modifiee
+    private float statWeight = 962, realWeight; //La seule stat du joueur qui ne demande pas d'etre modifiee
 
     private const float unitySpeedScalar = 13.879442142f; //Pour que la puissance correspondate vraiment a la vitesse maximale
     private const float maxRpm = 6000; //Les tours maximum pour le vilebrequin
@@ -26,7 +26,7 @@ public class VehicleControlScript : MonoBehaviour
     private float[] gearBox; //Les differents ratio dans la boite de vitesse
     private float[] gearThresholds; //La vitesse de chaque ratio a 5000rpm
     private int currentGear = 0, maxGear; //Le ratio actuellement selectionne, et le nombre de ratio dans la boite
-    private float gearSwitchTime = 0.6f; //Le temps qu'il faut pour passer une vitesse
+    private float gearSwitchTime = 0.4f; //Le temps qu'il faut pour passer une vitesse
     private float reverseTimer; //Combien de temps s'est ecoule depuis qu'on a demande a passer la marche arriere ?
 
     private float currentSpeed = 0, lostSpeed; //La vitesse (axe Z) actuelle du vehicule, et la vitesse qu'on perd lorsqu'on accelere pas
@@ -35,8 +35,10 @@ public class VehicleControlScript : MonoBehaviour
     private float steeringAngle = 0, wheelsAngle = 0; //L'angle du volant, et l'angle resultat pour les roues du vehicule
     private float steeringTurnSpeed = 1, steeringComeBackSpeed; //La vitesse a laquelle le volant atteint son maximum, et la vitesse a laquelle le volant retourne a son zero
 
-    private bool onTheGround;
+    private bool onTheGround, drivingOnTheGround, steeringOnTheGround; //Est-ce qu'on est au sol, est-ce que les roues motrices sont au sol, est-ce que les roues directrices sont au sol ?
     private float torqueScalar, forwardScalar, steeringScalar; //Les modifications du comportement en fonction des roues qui sont au sol (ou non)
+    private Vector3 forwardScalarVector = new Vector3(1f, 0f, 0f); //Le vecteur "tout droit" en fonction des roues motrices au sol
+    private float downwardMovement, downwardForce; //La force de la gravite, et notre mouvement vers le bas actuel
     #endregion
 
     #region Awake
@@ -101,6 +103,10 @@ public class VehicleControlScript : MonoBehaviour
     /// </summary>
     private void CalculateVariables()
     {
+        //"Calcul" du poids
+        realWeight = statWeight;
+        playerRigidbody.mass = realWeight;
+
         //Initialisation de la boite de vitesse
         gearBox = new float[] { 2.5f, 1.6f, 1.25f, 1f };
         maxGear = gearBox.Length;
@@ -132,16 +138,52 @@ public class VehicleControlScript : MonoBehaviour
         if (statHandling >= 50) steeringComeBackSpeed = 1 - 2.0484550067f + 0.6514417229f * Mathf.Log(statHandling);
         else steeringComeBackSpeed = 1.3f;
 
+        //Calcul de la force de la gravite sur le vehicule
+        downwardForce = 0.981f * realWeight;
     }
 
+    /// <summary>
+    /// Permet de gerer les effets de la gravite sur notre vehicule si on est pas au sol
+    /// </summary>
     private void GravityHandler()
     {
+        GatherWheelContacts();
+        CalculateGravity();
+    }
+
+    /// <summary>
+    /// On recupere les informations sur les roues qui sont au sol, et leur type
+    /// </summary>
+    private void GatherWheelContacts()
+    {
+        //On ordonne le calcul
         wheelsContact.ContactEffects();
-        
+
+        //Si on a pas toutes les roues motrices au sol, on ira pas "tout droit"
+        if (forwardScalar != wheelsContact.GetForwardScalar())
+        {
+            forwardScalar = wheelsContact.GetForwardScalar();
+            forwardScalarVector = new Vector3(0.7f + 0.3f * (1 - Mathf.Abs(forwardScalar)), 0f, 0.3f * Mathf.Abs(forwardScalar) * Mathf.Sign(forwardScalar));
+            forwardScalarVector.Normalize();
+        }
+
+        //On recupere les autres scalaires pour appliquer la puissance et le volant
         torqueScalar = wheelsContact.GetTorqueMultiplier();
-        forwardScalar = wheelsContact.GetForwardScalar();
         steeringScalar = wheelsContact.GetTurningScalar();
+
+        //On apprend quelle roue sont au sol, ou non
         onTheGround = wheelsContact.IsOnTheGround();
+        drivingOnTheGround = wheelsContact.IsDrivingOnTheGround();
+        steeringOnTheGround = wheelsContact.IsSteeringOnTheGround();
+    }
+
+    /// <summary>
+    /// Envoie le vehicule au sol si on est en l'air
+    /// </summary>
+    private void CalculateGravity()
+    {
+        if (onTheGround) downwardMovement = 0f;
+        else downwardMovement -= downwardForce * Time.fixedDeltaTime;
     }
 
     /// <summary>
@@ -149,42 +191,51 @@ public class VehicleControlScript : MonoBehaviour
     /// </summary>
     private void PedalsHandler()
     {
-        //Si on est pas en train de reculer, on a le fonctionnement normal
-        if (!isReversing)
+        //Si les roues motriches sont pas au sol, forcement on ralentit sans pouvoir utiliser les pedales
+        if (!drivingOnTheGround)
         {
-            //Est-ce qu'il est temps de changer de vitesse ?
-            if ((currentGear < maxGear - 1 && currentRpm + gainedRpm >= maxRpm) || (currentGear != 0 && currentSpeed < gearThresholds[currentGear - 1])) StartCoroutine(GearShift());
-
-            //On commence par choisir la bonne fonction pour gerer notre vitesse
-            if (pedalInput > 0 && !handbrakeInput)
-            {
-                if (isClutchEngaged) Accelerate();
-                else Decelerate(false);
-            }
-            else if (currentSpeed > 0)
-            {
-                if (pedalInput < 0) Decelerate(true);
-                else Decelerate(false);
-            }
-            //Si on a pas d'input pour acceler et qu'on est a l'arret, peut etre que le joueur veut reculer ?
-            else if (currentSpeed == 0 && pedalInput < 0 && !handbrakeInput && !isStartingReverse) StartCoroutine(ReverseShift());
+            Decelerate(false);
         }
-        //Sinon, on a un fonctionnement inverse etrange
+        //Sinon, le comportement est normal
         else
         {
-            //Est-ce qu'on a remplit les conditions pour repasser en marche avant ?
-            if(currentSpeed == 0 && pedalInput >= 0 && !handbrakeInput)
+            //Si on est pas en train de reculer, on a le fonctionnement normal
+            if (!isReversing)
             {
-                isReversing = false;
-                StartCoroutine(GearShift());
-            }
+                //Est-ce qu'il est temps de changer de vitesse ?
+                if ((currentGear < maxGear - 1 && currentRpm + gainedRpm >= maxRpm) || (currentGear != 0 && currentSpeed < gearThresholds[currentGear - 1])) StartCoroutine(GearShift());
 
-            //On oublie que ca fonctionne en inverse par ici
-            if (pedalInput < 0 && !handbrakeInput) Reverse();
-            else if(currentSpeed > 0)
+                //On commence par choisir la bonne fonction pour gerer notre vitesse
+                if (pedalInput > 0 && !handbrakeInput)
+                {
+                    if (isClutchEngaged) Accelerate();
+                    else Decelerate(false);
+                }
+                else if (currentSpeed > 0)
+                {
+                    if (pedalInput < 0) Decelerate(true);
+                    else Decelerate(false);
+                }
+                //Si on a pas d'input pour acceler et qu'on est a l'arret, peut etre que le joueur veut reculer ?
+                else if (currentSpeed == 0 && pedalInput < 0 && !handbrakeInput && !isStartingReverse) StartCoroutine(ReverseShift());
+            }
+            //Sinon, on a un fonctionnement inverse etrange
+            else
             {
-                if (pedalInput > 0) Decelerate(true);
-                else Decelerate(false);
+                //Est-ce qu'on a remplit les conditions pour repasser en marche avant ?
+                if (currentSpeed == 0 && pedalInput >= 0 && !handbrakeInput)
+                {
+                    isReversing = false;
+                    StartCoroutine(GearShift());
+                }
+
+                //On oublie que ca fonctionne en inverse par ici
+                if (pedalInput < 0 && !handbrakeInput) Accelerate();
+                else if (currentSpeed > 0)
+                {
+                    if (pedalInput > 0) Decelerate(true);
+                    else Decelerate(false);
+                }
             }
         }
 
@@ -198,20 +249,8 @@ public class VehicleControlScript : MonoBehaviour
     private void Accelerate()
     {
         //On commence par augmenter la vitesse moteur
-        gainedRpm = Time.fixedDeltaTime * ((realTorque * gearBox[currentGear]) / (realWeight * 1 * 1));
-        gainedRpm *= torqueScalar;
-        //On fait un check pour s'assurer qu'on respecte la realite
-        if (currentRpm + gainedRpm > maxRpm) currentRpm = maxRpm;
-        else currentRpm += gainedRpm;
-
-        //On peut maintenant deduire la vitesse du vehicule
-        CalculateSpeedFromRpm();
-    }
-
-    private void Reverse()
-    {
-        //On commence par augmenter la vitesse moteur, avec un ratio special
-        gainedRpm = Time.fixedDeltaTime * ((realTorque * (gearBox[0] + 1)) / (realWeight * 1 * 1));
+        if(!isReversing) gainedRpm = Time.fixedDeltaTime * ((realTorque * gearBox[currentGear]) / (realWeight * 1 * 1));
+        else gainedRpm = Time.fixedDeltaTime * ((realTorque * (gearBox[0] + 1)) / (realWeight * 1 * 1));
         gainedRpm *= torqueScalar;
         //On fait un check pour s'assurer qu'on respecte la realite
         if (currentRpm + gainedRpm > maxRpm) currentRpm = maxRpm;
@@ -228,7 +267,7 @@ public class VehicleControlScript : MonoBehaviour
     private void Decelerate(bool braking)
     {
         //On diminue la vitesse du vehicule
-        if(!isReversing) lostSpeed = Time.fixedDeltaTime * (((realDisplacement * gearBox[currentGear] + realBrake * Convert.ToSingle(braking)) * 1) / (realWeight * 1));
+        if (!isReversing) lostSpeed = Time.fixedDeltaTime * (((realDisplacement * gearBox[currentGear] + realBrake * Convert.ToSingle(braking)) * 1) / (realWeight * 1));
         else lostSpeed = Time.fixedDeltaTime * (((realDisplacement * (gearBox[0] + 1) + realBrake * Convert.ToSingle(braking)) * 1) / (realWeight * 1));
         //On fait un check pour s'assurer qu'on respecte la realite
         if (currentSpeed - lostSpeed < 0) currentSpeed = 0;
@@ -243,7 +282,7 @@ public class VehicleControlScript : MonoBehaviour
     /// </summary>
     private void CalculateSpeedFromRpm()
     {
-        if(!isReversing) currentSpeed = (currentRpm / maxRpm) * (realPower / gearBox[currentGear]);
+        if (!isReversing) currentSpeed = (currentRpm / maxRpm) * (realPower / gearBox[currentGear]);
         else currentSpeed = (currentRpm / maxRpm) * (realPower / (gearBox[0] + 1));
     }
 
@@ -252,7 +291,7 @@ public class VehicleControlScript : MonoBehaviour
     /// </summary>
     private void CalculateRpmFromSpeed()
     {
-        if(!isReversing) currentRpm = (currentSpeed * maxRpm * gearBox[currentGear]) / realPower;
+        if (!isReversing) currentRpm = (currentSpeed * maxRpm * gearBox[currentGear]) / realPower;
         else currentRpm = (currentSpeed * maxRpm * (gearBox[0] + 1)) / realPower;
     }
 
@@ -261,10 +300,13 @@ public class VehicleControlScript : MonoBehaviour
     /// </summary>
     private void ApplySpeed()
     {
-        if (!isReversing) playerRigidbody.velocity = transform.forward * currentSpeed * unitySpeedScalar * Time.fixedDeltaTime * (1 - 0.5f * forwardScalar)
-                + transform.right * currentSpeed * unitySpeedScalar * Time.fixedDeltaTime * (0.5f * forwardScalar);
+        if (!isReversing) playerRigidbody.velocity = transform.forward * currentSpeed * unitySpeedScalar * Time.fixedDeltaTime * forwardScalarVector.x
+                + Vector3.up * downwardMovement * Time.fixedDeltaTime
+                + transform.right * currentSpeed * unitySpeedScalar * Time.fixedDeltaTime * forwardScalarVector.z;
 
-        else playerRigidbody.velocity = -transform.forward * currentSpeed * unitySpeedScalar * Time.fixedDeltaTime;
+        else playerRigidbody.velocity = -transform.forward * currentSpeed * unitySpeedScalar * Time.fixedDeltaTime * forwardScalarVector.x
+                + Vector3.up * downwardMovement * Time.fixedDeltaTime
+                + transform.right * currentSpeed * unitySpeedScalar * Time.fixedDeltaTime * forwardScalarVector.z;
     }
 
     /// <summary>
@@ -274,7 +316,8 @@ public class VehicleControlScript : MonoBehaviour
     {
         CalculateSteeringAngle();
         CalculateWheelsAngle();
-        ApplySteering();
+        //On ne peut appliquer les changements du volant que si les roues directrices sont au sol
+        if(steeringOnTheGround) ApplySteering();
     }
 
     /// <summary>
@@ -308,8 +351,8 @@ public class VehicleControlScript : MonoBehaviour
     /// </summary>
     private void CalculateWheelsAngle()
     {
-        if (currentSpeed < 10) wheelsAngle = steeringAngle * realHandling * (currentSpeed / 10f);
-        else wheelsAngle = steeringAngle * realHandling;
+        if (currentSpeed < 10) wheelsAngle = (steeringAngle * realHandling + (steeringScalar / 3.6f) * realHandling) * (currentSpeed / 10f);
+        else wheelsAngle = steeringAngle * realHandling + (steeringScalar / 3.6f) * realHandling;
     }
 
     /// <summary>
@@ -317,7 +360,7 @@ public class VehicleControlScript : MonoBehaviour
     /// </summary>
     private void ApplySteering()
     {
-        if(!isReversing) transform.Rotate(Vector3.up, wheelsAngle * Time.fixedDeltaTime);
+        if (!isReversing) transform.Rotate(Vector3.up, wheelsAngle * Time.fixedDeltaTime);
         else transform.Rotate(Vector3.up, -wheelsAngle * Time.fixedDeltaTime);
     }
     #endregion
